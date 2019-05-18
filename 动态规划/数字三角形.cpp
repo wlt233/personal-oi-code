c5afcb28c15bc5d1f7b7b6966c9a29e9325d18d3
wlt233 <42347182+wlt233@users.noreply.github.com>
2019-05-18 15:04:49 +0800

    2019.5.18

---
+++ 动态规划/数字三角形.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n = 5;
+	//cin >> n;
+	int tri[5][5]=
+	{
+		{5,0,0,0,0},
+		{3,8,0,0,0},
+		{6,0,1,0,0},
+		{1,5,2,3,0},
+		{4,6,1,2,5}
+	};
+	int tri2[5][5];
+	for (int i = 0; i<=4; i++)
+		for (int j = 0; j<=4; j++)	
+			tri2[i][j] = tri[i][j];
+	
+
+	//max
+	for (int i = n-2; i>=0; i--) 
+		for (int j = 0; j <= i; j++) 
+			tri[i][j] = (tri[i+1][j] > tri[i+1][j+1]) ? tri[i][j]+tri[i+1][j] : tri[i][j]+tri[i+1][j+1];
+	
+	//min
+	for (int i = n-2; i>=0; i--)
+		for (int j = 0; j <= i; j++)
+			tri2[i][j] = (tri2[i+1][j] < tri2[i+1][j+1]) ? tri2[i][j]+tri2[i+1][j] : tri2[i][j]+tri2[i+1][j+1];
+	
+	cout << "max = " <<tri[0][0] << "  " << "min = " <<tri2[0][0] << endl;
+	
+	//max - out
+	int lo = 0;
+	for (int i = 0; i<=n-1; i++)
+	{
+		if (tri[i+1][lo] > tri[i+1][lo+1]) 
+		{
+			cout << tri[i][lo]-tri[i+1][lo] << endl;
+		} 
+		else
+		{
+			cout << tri[i][lo]-tri[i+1][lo+1] << endl;
+			lo++;
+		}
+	}
+	//min - out
+	int lo2 = 0;
+	for (int i = 0; i<=n-1; i++)
+	{
+		if (tri2[i+1][lo2] < tri2[i+1][lo2+1]) 
+		{
+			cout << tri2[i][lo2]-tri2[i+1][lo2] << endl;
+		} 
+		else
+		{
+			cout << tri2[i][lo2]-tri2[i+1][lo2+1] << endl;
+			lo2++;
+		}
+	}
+		 
+	
+	
+}
